6563dcd9856d17e9e69ee0102ead92089877dff6
IgorSinyak <igorsinyak92@gmail.com>
2016-12-14 20:22:22 +0300

    double quotes to single

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-var NAV_LIST_ITEM_SELECTOR = ".nav-list";
-var CONTACTS_SELECTOR = ".contact-icon";
-var POPUP_SELECTOR = ".popup";
-var NAV_BLOCK_ID = "#nav";
-var JOIN_BUTTON_ID = "#join"
-var EMAIL_INPUT_ID = "#your-mail";
-var SLIDER_PICTURE_ONE_ID = "#slider-picture1";
-var SLIDER_PICTURE_TWO_ID = "#slider-picture2";
-var SLIDER_BUTTON_ID = "#slider-button";
-var NAV_ACTIVE_CLASS = "nav-active";
-var CONTACT_ACTIVE_CLASS = "active";
-var POPUP_TEXT_ATTR = "data-popuptext";
+var NAV_LIST_ITEM_SELECTOR = '.nav-list';
+var CONTACTS_SELECTOR = '.contact-icon';
+var POPUP_SELECTOR = '.popup';
+var NAV_BLOCK_ID = '#nav';
+var JOIN_BUTTON_ID = '#join'
+var EMAIL_INPUT_ID = '#your-mail';
+var SLIDER_PICTURE_ONE_ID = '#slider-picture1';
+var SLIDER_PICTURE_TWO_ID = '#slider-picture2';
+var SLIDER_BUTTON_ID = '#slider-button';
+var NAV_ACTIVE_CLASS = 'nav-active';
+var CONTACT_ACTIVE_CLASS = 'active';
+var POPUP_TEXT_ATTR = 'data-popuptext';
 
 var navPosition;
 var navMenu;
@@ -30,19 +30,19 @@ $(document).ready( function() {
   navList.click(navScroll);
   
   $(CONTACTS_SELECTOR).click(function() {
-    var contArr = $(CONTACTS_SELECTOR);
+    var contactsArr = $(CONTACTS_SELECTOR);
     var popup = $(POPUP_SELECTOR);
     if ($(this).hasClass(CONTACT_ACTIVE_CLASS)) {
       $(this).removeClass(CONTACT_ACTIVE_CLASS);
       popup.hide();
       $(this).children().hide();
     } else {  
-    contArr.removeClass(CONTACT_ACTIVE_CLASS);
+    contactsArr.removeClass(CONTACT_ACTIVE_CLASS);
     $(this).addClass(CONTACT_ACTIVE_CLASS);
     var message = $(this).attr(POPUP_TEXT_ATTR);
     popup.show();
     popup.html(message);
-    contArr.children().hide();
+    contactsArr.children().hide();
     $(this).children().show();
     }
   });
@@ -62,10 +62,10 @@ $(document).ready( function() {
   });
 
   $(SLIDER_BUTTON_ID).click(function() {
-    var pictureOne =  $(SLIDER_PICTURE_ONE_ID).attr("src");
-    var pictureTwo =  $(SLIDER_PICTURE_TWO_ID).attr("src");
-    $(SLIDER_PICTURE_ONE_ID).attr("src", pictureTwo);
-    $(SLIDER_PICTURE_TWO_ID).attr("src", pictureOne);
+    var pictureOne =  $(SLIDER_PICTURE_ONE_ID).attr('src');
+    var pictureTwo =  $(SLIDER_PICTURE_TWO_ID).attr('src');
+    $(SLIDER_PICTURE_ONE_ID).attr('src', pictureTwo);
+    $(SLIDER_PICTURE_TWO_ID).attr('src', pictureOne);
   });
 
 });
@@ -75,15 +75,15 @@ function navScroll() {
   $(this).addClass(NAV_ACTIVE_CLASS);
   var id = $(this).attr('href');
   var top = $(id).offset().top - navMenuHeight;
-  $('body, html').animate({scrollTop:top}, "slow");
+  $('body, html').animate({scrollTop:top}, 'slow');
 }
 
 function positionToFixed() {
   var scrolled = $(window).scrollTop();
   if(scrolled >= navPosition) {
-    navMenu.css("position", "fixed");
+    navMenu.css('position', 'fixed');
   } else if (scrolled < navPosition) {
-    navMenu.css("position", "static");
+    navMenu.css('position', 'static');
   }
 }
 
